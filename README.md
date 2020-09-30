62fbba6bd9ded2e412df67138ce689940898d6df
Tu Nicole <nicole@TudeMacBook-Pro.local>
2020-09-30 20:10:40 +0800

    style guide

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Vue Simple Login [![VueJS Version](https://img.shields.io/badge/VueJS-v2-green.svg?style=flat-square)](https://vuejs.org/) [![Bulma Version](https://img.shields.io/badge/Bulma-v0.9.0-orange.svg?style=flat-square)](https://bulma.io/) [![License](https://img.shields.io/badge/License-MIT-red.svg?style=flat-square)](https://github.com/faisaltheparttimecoder/bulma-vuejs-demo-website/blob/master/LICENSE)
+# Vue Simple Inventory Analysis Website [![VueJS Version](https://img.shields.io/badge/VueJS-v2-green.svg?style=flat-square)](https://vuejs.org/) [![Bulma Version](https://img.shields.io/badge/Bulma-v0.9.0-orange.svg?style=flat-square)](https://bulma.io/) [![License](https://img.shields.io/badge/License-MIT-red.svg?style=flat-square)](https://github.com/faisaltheparttimecoder/bulma-vuejs-demo-website/blob/master/LICENSE)
 
-My first hands on experience on learning the css library [Bulma](https://bulma.io/) & JS Framework [VueJS](https://vuejs.org/).
-This website is a way to test out the features of the two libraries.
+My first hands on experience on learning the css library [Bulma](https://bulma.io/) & JS Framework [VueJS](https://vuejs.org/) & DB [firbase](https://firebase.google.com/).
+This website is a way to test out the features of these libraries.
 
 ## Build Setup / Installation
 
@@ -19,9 +19,29 @@ yarn install
 yarn run dev
 
 # Once started, on your browser navigate to the below URL
-http://localhost:8000
+http://localhost:8001
 
 # build for production with minification
 npm run build
 
 ```
+
+## The features is not completed.
+
+### Todo these:
+##### inventory
+  <li><input type="checkbox" /> 加brand price </li>
+  <li><input type="checkbox" /> 上傳圖片</li>
+
+##### report
+<li><input type="checkbox" /> 統計 by 月 年 </li>
+<li><input type="checkbox" /> 下載excel or 寄信</li>
+
+##### dashboard 
+<li><input type="checkbox" /> 計算買太多東西的alert</li>
+<li><input type="checkbox" /> 快要沒有庫存的alert</li>
+<li><input type="checkbox" /> 各類的庫存圖表</li>
+
+##### setting
+<li><input type="checkbox" /> 設定寄信通知補貨通知</li>
+<li><input type="checkbox" /> 上傳個人照片</li>
--- src/views/StyleGuide.vue
+++ src/views/StyleGuide.vue
@@ -1,118 +1,206 @@
 <template>
-	<section class="is-fullheight">
-		<nav-bar />
-		<div class="hero-body main-content">
-			<div class="box container">
-				<div class="tabs is-boxed">
-					<ul>
-						<li
-							v-for="(t,i) in tabComponents"
-							:key="i"
-							:class="{'is-active':currentTab === t.id}"
-						>
-							<a @click="changeTab(t.id)">
-								<i :class="t.icon" />
-								{{ t.name }}</a>
-						</li>
-					</ul>
-				</div>
-				<div class="tab-contents">
-					<div
-						class="tab-content"
-						:class="{'is-active':currentTab === 0}"
-					>
-						<ul class="color-list">
-							<li class="color-card">
-								<div class="card">
-									<div class="card-content has-background-primary">
-									</div>
-									<footer class="card-footer">
-										<b>Primary</b>
-										<br />#174B81
-									</footer>
-								</div>
-							</li>
-							<li class="color-card">
-								<div class="card">
-									<div class="card-content has-background-link">
-									</div>
-									<footer class="card-footer">
-										<b>Link</b>
-										<br />#263056
-									</footer>
-								</div>
-							</li>
-							<li class="color-card">
-								<div class="card">
-									<div class="card-content has-background-info">
-									</div>
-									<footer class="card-footer">
-										<b>Info</b>
-										<br />#A6B8D0
-									</footer>
-								</div>
-							</li>
-						</ul>
-					</div>
-					<div
-						class="tab-content"
-						:class="{'is-active':currentTab === 1}"
-					>
-						Typography
-					</div>
-					<div
-						class="tab-content"
-						:class="{'is-active':currentTab == 2}"
-					>
-						Buttons
-					</div>
-					<div
-						class="tab-content"
-						:class="{'is-active':currentTab == 3}"
-					>
-						Components
-					</div>
-					<div
-						class="tab-content"
-						:class="{'is-active':currentTab == 4}"
-					>
-						Form
-					</div>
-					<div
-						class="tab-content"
-						:class="{'is-active':currentTab == 5}"
-					>
-						Table
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+  <section class="is-fullheight">
+    <nav-bar />
+    <div class="hero-body main-content">
+      <div class="box container">
+        <div class="tabs is-boxed">
+          <ul>
+            <li v-for="(t,i) in tabComponents" :key="i" :class="{'is-active':currentTab === t.id}">
+              <a @click="changeTab(t.id)">
+                <i :class="t.icon" />
+                {{ t.name }}
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="tab-contents">
+          <div class="tab-content" :class="{'is-active':currentTab === 0}">
+            <ul class="color-list mt-6">
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content" style="background-color:#83888B">
+                    <span>#83888B</span>
+                  </div>
+                  <footer class="card-footer">Background</footer>
+                </div>
+              </li>
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content" style="background-color:#A58E80">
+                    <span>#A58E80</span>
+                  </div>
+                  <footer class="card-footer">Body Background</footer>
+                </div>
+              </li>
+            </ul>
+            <ul class="color-list mt-5">
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-primary">
+                    <span>#174B81</span>
+                  </div>
+                  <footer class="card-footer">Primary</footer>
+                </div>
+              </li>
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-link">
+                    <span>#263056</span>
+                  </div>
+                  <footer class="card-footer">Link</footer>
+                </div>
+              </li>
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-info">
+                    <span>#A6B8D0</span>
+                  </div>
+                  <footer class="card-footer">Info</footer>
+                </div>
+              </li>
+            </ul>
+            <ul class="color-list mt-5">
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-dark">
+                    <span>#2A2A34</span>
+                  </div>
+                  <footer class="card-footer">Dark</footer>
+                </div>
+              </li>
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-grey-dark">
+                    <span>#363837</span>
+                  </div>
+                  <footer class="card-footer">Grey Dark</footer>
+                </div>
+              </li>
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-grey-light">
+                    <span>#A49E9E</span>
+                  </div>
+                  <footer class="card-footer">Grey Light</footer>
+                </div>
+              </li>
+            </ul>
+            <ul class="color-list mt-5">
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-white">
+                    <span class="has-text-black">#F0EDE4</span>
+                  </div>
+                  <footer class="card-footer">White</footer>
+                </div>
+              </li>
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-danger">
+                    <span>#77202F</span>
+                  </div>
+                  <footer class="card-footer">Danger</footer>
+                </div>
+              </li>
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-warning">
+                    <span>#BA9238</span>
+                  </div>
+                  <footer class="card-footer">Warning</footer>
+                </div>
+              </li>
+              <li class="color-card">
+                <div class="card">
+                  <div class="card-content has-background-success">
+                    <span>#255958</span>
+                  </div>
+                  <footer class="card-footer">Success</footer>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="tab-content" :class="{'is-active':currentTab === 1}">
+            <h1>H1 Heading</h1>3rem
+            <h2>H2 Heading</h2>2.5rem
+            <h3>H3 Heading</h3>2rem
+            <h4>H4 Heading</h4>1.5rem
+            <h5>H5 Heading</h5>1.25rem
+            <h6>H6 Heading</h6>1rem
+            <div class="is-size-7">H7 Heading</div>0.75rem
+          </div>
+          <div class="tab-content" :class="{'is-active':currentTab == 2}">
+            <h5 class="mb-5"># Default</h5>
+            <div class="buttons mb-5">
+              <a class="button">Anchor</a>
+              <button class="button">Button</button>
+              <input class="button" type="submit" value="Submit input" />
+              <input class="button" type="reset" value="Reset input" />
+            </div>
+            <h5 class="mb-5"># Colors</h5>
+            <div class="buttons">
+              <button class="button is-white">White</button>
+              <button class="button is-light">Light</button>
+              <button class="button is-dark">Dark</button>
+              <button class="button is-black">Black</button>
+              <button class="button is-text">Text</button>
+            </div>
+            <div class="buttons">
+              <button class="button is-primary">Primary</button>
+              <button class="button is-link">Link</button>
+              <button class="button is-info">Info</button>
+              <button class="button is-success">Success</button>
+              <button class="button is-warning">Warning</button>
+              <button class="button is-danger">Danger</button>
+            </div>
+            <div class="buttons mb-5">
+              <button class="button is-primary is-light">Primary</button>
+              <button class="button is-link is-light">Link</button>
+              <button class="button is-info is-light">Info</button>
+              <button class="button is-success is-light">Success</button>
+              <button class="button is-warning is-light">Warning</button>
+              <button class="button is-danger is-light">Danger</button>
+            </div>
+            <h5 class="mb-5"># Styles</h5>
+            <div class="buttons mb-5">
+              <button class="button is-outlined">Outlined</button>
+              <button class="button is-primary is-inverted">Inverted</button>
+              <button class="button is-primary is-inverted is-outlined">Invert Outlined</button>
+              <button class="button is-rounded">Rounded</button>
+            </div>
+          </div>
+          <div class="tab-content" :class="{'is-active':currentTab == 3}">Components</div>
+          <div class="tab-content" :class="{'is-active':currentTab == 4}">Form</div>
+          <div class="tab-content" :class="{'is-active':currentTab == 5}">Table</div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import NavBar from '@views/layouts/NavBar';
 
 export default {
-	name: 'StyleGuide',
-	components: { NavBar },
-	data() {
-		return {
-			currentTab: 0,
-			tabComponents: [
-				{ id: 0, name: 'Color Palette', icon: 'fas fa-palette' },
-				{ id: 1, name: 'Typography', icon: 'fas fa-i-cursor' },
-				{ id: 2, name: 'Buttons', icon: 'fas fa-hand-point-up' },
-				{ id: 3, name: 'Components', icon: 'far fa-object-group' },
-				{ id: 4, name: 'Form', icon: 'fab fa-wpforms' },
-				{ id: 5, name: 'Table', icon: 'fas fa-table' }
-			]
-		}
-	},
-	methods: {
-		changeTab(tab) {
-			this.currentTab = tab;
-		}
-	},
+  name: 'StyleGuide',
+  components: { NavBar },
+  data() {
+    return {
+      currentTab: 0,
+      tabComponents: [
+        { id: 0, name: 'Color Palette', icon: 'fas fa-palette' },
+        { id: 1, name: 'Typography', icon: 'fas fa-i-cursor' },
+        { id: 2, name: 'Buttons', icon: 'fas fa-hand-point-up' },
+        { id: 3, name: 'Components', icon: 'far fa-object-group' },
+        { id: 4, name: 'Form', icon: 'fab fa-wpforms' },
+        { id: 5, name: 'Table', icon: 'fas fa-table' }
+      ]
+    }
+  },
+  methods: {
+    changeTab(tab) {
+      this.currentTab = tab;
+    }
+  },
 }
 </script>
