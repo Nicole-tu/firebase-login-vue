d3da7dfbffdc9920cab78c23d6485e8cfb1d2e0a
Nicole Tu 杜晨寧 <1801665@C0699.local>
2020-10-05 16:55:07 +0800

    mixin fix

--- src/mixins/index.js
+++ src/mixins/index.js
@@ -4,27 +4,8 @@ const formatDate = (date, formater) => {
   return format(new Date(date), formater)
 }
 
-const getSiblings = function (e) {
-  // for collecting siblings
-  let siblings = [];
-  // if no parent, return no sibling
-  if (!e.parentNode) {
-    return siblings;
-  }
-  // first child of the parent node
-  let sibling = e.parentNode.firstChild;
-  // collecting siblings
-  while (sibling) {
-    if (sibling.nodeType === 1 && sibling !== e) {
-      siblings.push(sibling);
-    }
-    sibling = sibling.nextSibling;
-  }
-  return siblings;
-};
-
 export default {
   methods: {
-    formatDate, getSiblings
+    formatDate
   }
 }
